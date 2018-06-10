dc7f19b2a15d49b5dc77b60ff79ce043e29710e2
Student <student@d17-l427-pc-09.ipa.iisg.agh.edu.pl>
2018-06-10 12:10:25 +0200

    dodane ustawianie własnej liczby

--- app/src/main/java/com/example/licznik/MainActivity.java
+++ app/src/main/java/com/example/licznik/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.licznik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,33 +17,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private void updateNumber() {
+        TextView text = findViewById(R.id.counter);
+        String newNumber;
+        newNumber = String.valueOf(counter);
+        text.setText(newNumber);
+    }
+
     public void zwieksz(View viev){
 
         counter++;
-        TextView text = findViewById(R.id.counter);
-        String newNumber;
-        newNumber = String.valueOf(counter);
-        text.setText(newNumber);
-
-
+        updateNumber();
     }
+
     public void zmniejsz(View viev){
 
         counter--;
-        TextView text = findViewById(R.id.counter);
-        String newNumber;
-        newNumber = String.valueOf(counter);
-        text.setText(newNumber);
-
-
+        updateNumber();
     }
     public void wyzeruj(View viev){
 
         counter=0;
+        updateNumber();
+    }
+
+    public void wlasnaLiczba(View view){
+
+        EditText editText = (EditText) findViewById(R.id.editNumber);
+        Editable text1= editText.getText();
+        String newNumber = String.valueOf(text1);
         TextView text = findViewById(R.id.counter);
-        String newNumber;
-        newNumber = String.valueOf(counter);
+
         text.setText(newNumber);
+        counter = Integer.parseInt(editText.getText().toString());
 
 
     }
